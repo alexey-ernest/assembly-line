e7f4c726d930023c8d616cebb64b92b3979d753c
alexey-ernest <alexey.ernest@gmail.com>
2015-04-26 13:06:02 +0300

    Employee Migrations

---
+++ AssemblyLine/Migrations/201504261003221_RequiredEmployeeFields.cs
@@ -0,0 +1,20 @@
+namespace AssemblyLine.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RequiredEmployeeFields : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Employees", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Employees", "LastName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Employees", "LastName", c => c.String());
+            AlterColumn("dbo.Employees", "FirstName", c => c.String());
+        }
+    }
+}
